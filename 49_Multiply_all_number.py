d213e0388a27cb9c934b994c04aec422285ed2f1
Ansh pandey <akashakashpandey612@gmail.com>
2026-07-08 09:57:03 +0530

    Added a Python script to multiply all numbers in a list and print the product.

---
+++ 49_Multiply_all_number.py
@@ -0,0 +1,18 @@
+# sample list of numbers 
+
+numbers = [10,20,30,40,50]
+
+# Initialize a variable to store the product 
+
+product_of_numbers = 1 
+
+# Iterate through the list and accumulate the product 
+
+for i in numbers:
+    product_of_numbers *= i
+
+    # print the product 
+
+    print("The product of all the numbers in the list is:", product_of_numbers) 
+
+
