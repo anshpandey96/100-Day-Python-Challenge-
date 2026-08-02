e7a048d0390d768bb5de5f791b9a67286dfa5a52
Ansh pandey <akashakashpandey612@gmail.com>
2026-08-02 20:53:17 +0530

    Added code to calculate the sum of all items in a dictionary

---
+++ 72_tt.py
@@ -0,0 +1,14 @@
+# Sample dictionary
+sample_dict = {'apple': 3, 'banana': 1, 'cherry': 2, 'date': 4}
+
+# Sort by Keys
+sorted_dict_by_keys = dict(sorted(sample_dict.items()))
+print("Sorted by keys:")
+for key, value in sorted_dict_by_keys.items():
+    print(f"{key}: {value}")
+
+# Sort by Values
+sorted_dict_by_values = dict(sorted(sample_dict.items(), key=lambda item: item[1]))
+print("\nSorted by values:")
+for key, value in sorted_dict_by_values.items():
+    print(f"{key}: {value}")
