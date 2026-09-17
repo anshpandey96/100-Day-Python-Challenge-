ddb00aa86b4a7e14550af2555cc4984d15f2917a
Ansh pandey <akashakashpandey612@gmail.com>
2026-09-17 19:47:19 +0530

    Swapped two variables without using a temp variable

---
+++ 107_tast.py
@@ -0,0 +1,13 @@
+# Program to swap two variables without using a temp variable
+
+a = 5
+b = 10
+
+# Swapping using tuple unpacking
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+
+
